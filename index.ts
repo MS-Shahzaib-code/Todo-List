5403a18444558c01efac9d40712d06a559d16ba3
MS-Shahzaib-code <160013511+MS-Shahzaib-code@users.noreply.github.com>
2024-04-05 04:49:53 -0700

    upload my todo on git

---
+++ index.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer"
+import modules from "inquirer"  // that means inquirer import from modules 
+
+let todo = []
+let reapesct_Q = true;
+while(reapesct_Q){
+    let todo_List = await inquirer.prompt([
+    {
+        name  : "frist_Add",
+        type : "input",
+        message : "what would you like to add in your todo?"
+    },
+    {
+       name : "second_Add",
+       type : "confirm",
+       message : "would you like to add more in your todo?",
+       default : "ture"
+    }
+
+  ])  
+todo.push(todo_List.frist_Add)
+reapesct_Q = todo_List.second_Add
+// console.log(todo_List)
+console.log(todo)
+}
